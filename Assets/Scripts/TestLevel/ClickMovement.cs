1e86d0eca6b798fa7153df7c0d06fad77aee8717
stefanplc <stefanplc@gmail.com>
2016-09-26 18:54:48 +0300

    Character Model, Movement Script, Test Arena
    I added the character model with idle and movement animations. I updated
    both the existing movement script so it uses the animations but I also
    added a secondary one based on a tutorial (see code comments). I also
    added a test arena for us to use.

--- Assets/Scripts/TestLevel/ClickMovement.cs
+++ Assets/Scripts/TestLevel/ClickMovement.cs
@@ -3,16 +3,16 @@ using System.Collections;
 
 public class ClickMovement : MonoBehaviour {
 
-	public Player player;
+	public Player player; //Attach the player object
     
-    void OnMouseDown ()
+    void OnMouseDown () //Start action when the mouse is clicked
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         
         Physics.Raycast(ray, out hit);
         
-        if(hit.collider.gameObject == gameObject)
+        if(hit.collider.gameObject == gameObject) //The script is attached to the plane so this detects collision with the plane as the gameObject
         {
             Vector3 newTarget = hit.point + new Vector3(0, 0.5f, 0);
             player.Target = newTarget;
---
+++ Assets/Scripts/TestLevel/ClickToMove.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToMove : MonoBehaviour {
+
+    //based on https://www.youtube.com/watch?v=k7yx7D6MU6w
+
+    [SerializeField] [Range(1, 20)]
+    private float speed = 1/20;                   //how fast the player moves.
+
+    private Vector3 targetPosition;             //where we want to travel too.
+    private bool isMoving;                      //toggle to check track if we are moving or not.
+
+    const int LEFT_MOUSE_BUTTON = 0;            //a more visual description of what the left mouse button code is.
+
+    private Animator anim;                      // The Animator is what controls the switching from one animator to the other
+
+
+
+    /// <summary>
+    /// Use this for initialization.
+    /// </summary>
+    void Start()
+    {
+        targetPosition = transform.position;        //set the target postion to where we are at the start
+        isMoving = false;                           //set  out move toggle to false.
+        anim = gameObject.GetComponentInChildren<Animator>(); // I need to get the animator component to have access to it
+    }
+
+
+
+    /// <summary>
+    /// Detect the player input every frame
+    /// </summary>
+    void Update()
+    {
+        //if the player clicked on the screen, find out where
+        if (Input.GetMouseButton(LEFT_MOUSE_BUTTON))
+        {
+            anim.SetInteger("AnimationParameter", 1); // When the AnimationParameter is set to 1, the walk animation is being played.
+            SetTargetPosition();
+        }
+
+        //if we are still moving, then move the player
+        if (isMoving)
+        {
+            MovePlayer();
+        } else {
+            anim.SetInteger("AnimationParameter", 0); // AnimationParameter is an integer that's used to switch between animations. When it's set to 0, the idle animation is being played.
+        }
+            
+    }
+
+    /// <summary>
+	/// Sets the target position we will travel too.
+	/// </summary>
+	void SetTargetPosition()
+    {
+        Plane plane = new Plane(Vector3.up, transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float point = 0f;
+
+        if (plane.Raycast(ray, out point))
+        {
+            targetPosition = ray.GetPoint(point);
+        }
+            
+
+        //set the ball to move
+        isMoving = true;
+
+    }
+
+
+
+    /// <summary>
+    /// Moves the player in the right direction and also rotates them to look at the target position.
+    /// When the player gets to the target position, stop them from moving.
+    /// </summary>
+    void MovePlayer()
+    {
+        transform.LookAt(targetPosition);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+        //if we are at the target position, then stop moving
+        if (transform.position == targetPosition)
+        {
+            isMoving = false;
+        }
+            
+
+        Debug.DrawLine(transform.position, targetPosition, Color.red);
+    }
+
+
+}
