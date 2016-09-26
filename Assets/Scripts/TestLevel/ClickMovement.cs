5248924e3e6bbac329753a1c0eb8bcb8ed9f3e36
John W <john.d.wright@gmail.com>
2016-09-25 23:51:19 -0700

    Test Scene
    Created a test scene with click movement

---
+++ Assets/Scripts/TestLevel/ClickMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickMovement : MonoBehaviour {
+
+	public Player player;
+    
+    void OnMouseDown ()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        
+        Physics.Raycast(ray, out hit);
+        
+        if(hit.collider.gameObject == gameObject)
+        {
+            Vector3 newTarget = hit.point + new Vector3(0, 0.5f, 0);
+            player.Target = newTarget;
+        }
+    }
+}
---
+++ Assets/Scripts/TestLevel/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float smoothing = 7f;
+    public Vector3 Target
+    {
+        get { return target; }
+        set
+        {
+            target = value;
+
+            StopCoroutine("Movement");
+            StartCoroutine("Movement", target);
+        }
+    }
+
+
+    private Vector3 target;
+
+
+    IEnumerator Movement(Vector3 target)
+    {
+        while (Vector3.Distance(transform.position, target) > 0.05f)
+        {
+            transform.position = Vector3.Lerp(transform.position, target, smoothing * Time.deltaTime);
+
+            yield return null;
+        }
+    }
+}
