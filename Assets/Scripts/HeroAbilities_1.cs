e20f5d438e8c4f81e55f65e3af7eb4c9499c6100
stefanplc <stefanplc@gmail.com>
2016-10-03 03:00:34 +0300

    UI + Earth Mage Animations
    Implemented the UI and started coding it. I also started building the
    architecture with how the abilities will work together with the UI. All
    of these are happening in a new separate level. I also added 3 new
    animations for the Earth Mage for the 3 basic abilities.. The variables
    for them need to change to 2, 3 and 4 where 0 is idle (which was also
    updated) and 1 is walking.

---
+++ Assets/Scripts/HeroAbilities_1.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroAbilities_1 : MonoBehaviour {
+
+    public void AbilityOneActions() // All the actions for this ability
+    {
+        Debug.Log("Hero 1: Ability 1");
+    }
+
+
+    public void AbilityTwoActions() // All the actions for this ability
+    {
+        Debug.Log("Hero 1: Ability 2");
+    }
+
+
+    public void AbilityThreeActions() // All the actions for this ability
+    {
+        Debug.Log("Hero 1: Ability 3");
+    }
+
+
+    public void AbilityFourActions() // All the actions for this ability
+    {
+        Debug.Log("Hero 1: Ability 4");
+    }
+
+}
---
+++ Assets/Scripts/HeroAbilities_4.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroAbilities_4 : MonoBehaviour {
+
+    public void AbilityOneActions() // All the actions for this ability
+    {
+        Debug.Log("Hero 4: Ability 1");
+    }
+
+
+    public void AbilityTwoActions() // All the actions for this ability
+    {
+        Debug.Log("Hero 4: Ability 2");
+    }
+
+
+    public void AbilityThreeActions() // All the actions for this ability
+    {
+        Debug.Log("Hero 4: Ability 3");
+    }
+
+
+    public void AbilityFourActions() // All the actions for this ability
+    {
+        Debug.Log("Hero 4: Ability 4");
+    }
+}
