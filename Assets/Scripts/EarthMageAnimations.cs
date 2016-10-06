94c178a248b7b15eefd155389aaeb7e1e971d9b4
stefanplc <stefanplc@gmail.com>
2016-10-06 05:02:10 +0300

    Earth Mage Animations Part 4
    Updated how the extra animation elements (spikes, rocks etc..) are built
    so they'll be easier to code. I separated them as new objects in the
    scene so they can be easily positioned, enaabled/disabled and etc...

--- Assets/Scripts/EarthMageAnimations.cs
+++ Assets/Scripts/EarthMageAnimations.cs
@@ -3,18 +3,6 @@ using System.Collections;
 
 public class EarthMageAnimations : MonoBehaviour {
 
-    // There are a bunch of animation models that I need to hide and then enable only for certain animations. I'm linking them here as game objects so I can turn them on when needed, they're disabled by default.
-    public GameObject GreenItemOne;
-    public GameObject GreenItemTwo;
-    public GameObject GreenItemThree;
-    public GameObject GreenItemFour;
-
-    public GameObject AutoAttacksItemOne;
-    public GameObject AutoAttacksItemTwo;
-    public GameObject AutoAttacksItemThree;
-    public GameObject AutoAttacksItemFour;
-    public GameObject AutoAttacksItemFive;
-
 
     // These will be turned on in the inspector to specify which animation I want the model to play.
     public bool Idle;
@@ -42,10 +30,6 @@ public class EarthMageAnimations : MonoBehaviour {
         if (Green)
         {
             anim.SetInteger("AnimationPosition", 1);
-            GreenItemOne.gameObject.SetActive(true);
-            GreenItemTwo.gameObject.SetActive(true);
-            GreenItemThree.gameObject.SetActive(true);
-            GreenItemFour.gameObject.SetActive(true);
         }
 
         if (Blue)
@@ -66,11 +50,6 @@ public class EarthMageAnimations : MonoBehaviour {
         if (AutoAttacks)
         {
             anim.SetInteger("AnimationPosition", 5);
-            AutoAttacksItemOne.gameObject.SetActive(true);
-            AutoAttacksItemTwo.gameObject.SetActive(true);
-            AutoAttacksItemThree.gameObject.SetActive(true);
-            AutoAttacksItemFour.gameObject.SetActive(true);
-            AutoAttacksItemFive.gameObject.SetActive(true);
         }
 
         if (Walking)
